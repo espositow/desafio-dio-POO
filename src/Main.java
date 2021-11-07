c3a36024c7d771553f5655f9e13bb28532f443a9
espositow <espositow@gmail.com>
2021-11-07 00:25:29 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import com.github.espositow.desafio.dominio.Curso;
-import com.github.espositow.desafio.dominio.Mentoria;
+import com.github.espositow.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,8 +20,36 @@ public class Main {
         mentoria.setDescricao("descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devEspositow = new Dev();
+        devEspositow.setNome("Espositow");
+        devEspositow.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Espositow:" + devEspositow.getConteudosInscritos());
+
+        devEspositow.progredir();
+        devEspositow.progredir();
+        System.out.println("Conteúdos Inscritos Espositow:" + devEspositow.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Espositow:" + devEspositow.getConteudosConcluidos());
+        System.out.println("XP:" + devEspositow.calcularTotalXp());
+
+        System.out.println("--------");
+
+        Dev devHevellyn = new Dev();
+        devHevellyn.setNome("Hevellyn");
+        devHevellyn.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Hevellyn:" + devHevellyn.getConteudosInscritos());
+
+        devHevellyn.progredir();
+        devHevellyn.progredir();
+        devHevellyn.progredir();
+        System.out.println("Conteúdos Inscritos Hevellyn:" + devHevellyn.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Hevellyn:" + devHevellyn.getConteudosConcluidos());
+        System.out.println("XP:" + devHevellyn.calcularTotalXp());
     }
 }
